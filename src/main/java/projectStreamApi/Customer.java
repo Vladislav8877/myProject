49e7c9876a7a0427c167e39caa3ad20d6570657a
Vlad Azarov <vladazarov44@gmail.com>
2026-05-28 11:48:39 +0300

    feat: add Stream API project tasks 1-15

---
+++ src/main/java/projectStreamApi/Customer.java
@@ -0,0 +1,26 @@
+package projectStreamApi;
+import java.util.Set;
+public class Customer {
+
+    private final long id;
+    private final String name;
+    private final long level;
+    private final Set<Order> orders;
+
+    Customer(long id, String name, long level, Set<Order> orders) {
+        this.id = id;
+        this.name = name;
+        this.level = level;
+        this.orders = orders;
+    }
+
+    public long getId() {return id;}
+    public String getName() {return name;}
+    public long getLevel() {return level;}
+    public Set<Order> getOrders() {return orders;}
+
+    @Override
+    public String toString() {
+        return String.format("ID: %d | Name: %s | Level: %d", id, name, level);
+    }
+}
